1dc13dfc16a47c2170c92a3c8252e9431b7d1278
mffonua <108596346+mffonua@users.noreply.github.com>
2022-09-19 21:10:44 -0600

    adding in base and installing npm inquirer

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
 
+
+
+
+// Function to promt questions
+function inquirerPrompts() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers)
+        writeToFile(answers);
+        })
+}
+// Function to write README file
+function writeToFile(data) {
+    fs.writeFile('MockupREADME.md', generateMarkdown(data),
+    err ? console.log(err) : console.log('Success'));
+}
+// Function to initialize app
+function init() {
+    inquirerPrompts
+}
 // Function call to initialize app
 init();
